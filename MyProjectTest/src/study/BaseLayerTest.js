827bc7740b08194809f346e6d76bc06cb2ed9993
Min Nguyen <minnguyen0507@gmail.com>
2017-08-18 23:50:40 +0700

    #Fix : Base Layer

--- MyProjectTest/src/study/BaseLayerTest.js
+++ MyProjectTest/src/study/BaseLayerTest.js
@@ -2,22 +2,21 @@
  * Created by Admin on 17/8/2017.
  */
 var BaseLayerTest = cc.Layer.extend({
-    ctor : function () {
-      this._super();
-
-      var size = cc.winSize;
-
-      var spriteTest = new cc.Sprite(res.btn_next);
-      this.addChild(spriteTest,1);
-      spriteTest.setPosition(size.width/2, size.height/2);
-      spriteTest.setAnchorPoint(0.5, 0.5);
+    ctor:function () {
+        this._super();
+        this.init();
     },
-
-    testLayer : function () {
+    init : function(){
         var size = cc.winSize;
-        var bgTest = new cc.Sprite(res.bg_test);
-        this.addChild(bgTest,1);
-        bgTest.setPosition(size.width/2, size.height/2);
-        bgTest.setAnchorPoint(0.5, 0.5);
+
+        var spriteTest = new cc.Sprite(res.btn_next);
+        this.addChild(spriteTest,1);
+        spriteTest.setPosition(size.width/2, size.height/2);
+        spriteTest.setAnchorPoint(0.5, 0.5);
     },
+
+    callFunction : function () {
+       //Xu li gi o day cung duoc
+       // tạo hàm, đầu hàm phải là động từ
+    }
 });
