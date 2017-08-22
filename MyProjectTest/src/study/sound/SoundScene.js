5a1c70e3ef700062fcab6742d04a91f0e2303503
minhnc <minnguyen0507@gmail.com>
2017-08-22 09:10:00 +0700

    #Fix : dùng "cc.p" thay vì "new cc.Point"

--- MyProjectTest/src/study/sound/SoundScene.js
+++ MyProjectTest/src/study/sound/SoundScene.js
@@ -9,29 +9,30 @@ var SoundLayer = cc.Layer.extend({
         cc.log("vao Scene Sound");
 
         this.audioEngine = cc.audioEngine;
-        var playerSoundMenu = new cc.MenuItemFont.create("Player Sound effect", this.playerSound, this);
-        playerSoundMenu.setPosition(new cc.Point(0, 350));
+        this.audioEngine = cc.audioEngine;
+        var playerSoundMenu = new cc.MenuItemFont("Player Sound effect", this.playerSound, this);
+        playerSoundMenu.setPosition(cc.p(0, 350));
 
-        var playerBgMusicMenu = new cc.MenuItemFont.create("Play BG Music", this.playBGMusic, this);
-        playerBgMusicMenu.setPosition(new cc.Point(0, 300));
+        var playerBgMusicMenu = new cc.MenuItemFont("Play BG Music", this.playBGMusic, this);
+        playerBgMusicMenu.setPosition(cc.p(0, 300));
 
-        var stopBGMusicMenu = new cc.MenuItemFont.create("Stop BG Music", this.stopBGMusic, this);
-        stopBGMusicMenu.setPosition(new cc.Point(0, 250));
+        var stopBGMusicMenu = new cc.MenuItemFont("Stop BG Music", this.stopBGMusic, this);
+        stopBGMusicMenu.setPosition(cc.p(0, 250));
 
-        var musicUpMenu = new cc.MenuItemFont.create("Music volume Up",this.musicUp,this);
-        musicUpMenu.setPosition(new cc.Point(0,200));
+        var musicUpMenu = new cc.MenuItemFont("Music volume Up",this.musicUp,this);
+        musicUpMenu.setPosition(cc.p(0,200));
 
-        var musicDownMenu = new cc.MenuItemFont.create("Music volume Down",this.musicDown,this);
-        musicDownMenu.setPosition(new cc.Point(0,150));
+        var musicDownMenu = new cc.MenuItemFont("Music volume Down",this.musicDown,this);
+        musicDownMenu.setPosition(cc.p(0,150));
 
-        var effectsUpMenu = new cc.MenuItemFont.create("Effects volume Up",this.effectsUp,this);
-        effectsUpMenu.setPosition(new cc.Point(0,100));
+        var effectsUpMenu = new cc.MenuItemFont("Effects volume Up",this.effectsUp,this);
+        effectsUpMenu.setPosition(cc.p(0,100));
 
-        var effectsDownMenu = new cc.MenuItemFont.create("Effects volume Down",this.effectsDown,this);
-        effectsDownMenu.setPosition(new cc.Point(0,50));
+        var effectsDownMenu = new cc.MenuItemFont("Effects volume Down",this.effectsDown,this);
+        effectsDownMenu.setPosition(cc.p(0,50));
 
         var menu = cc.Menu.create(playerSoundMenu, playerBgMusicMenu, stopBGMusicMenu, musicUpMenu, musicDownMenu, effectsUpMenu, effectsDownMenu);
-        menu.setPosition(new cc.Point(400, 140));
+        menu.setPosition(cc.p(400, 140));
         this.addChild(menu);
         return true;
     },
