eb3342c1a7b556433544f8f52963176cda274d27
minnguyen0507 <minnguyen0507@gmail.com>
2017-10-23 17:21:06 +0700

    #Update - UICocos2d: thêm action cho progressTimer!

--- MyProjectTest/src/study/ccui/UICocos2D.js
+++ MyProjectTest/src/study/ccui/UICocos2D.js
@@ -75,8 +75,34 @@ var GUIUICocos2d = AdminBaseGUI.extend({
         testProgressTimer.setMidpoint(cc.p(0,0));
         testProgressTimer.setPosition(cc.winSize.width/2, cc.winSize.height/2);
         this.addChild(testProgressTimer, 1);
-        var to1 = cc.progressTo(5, 0); //trong vòng 5s chạy từ 100-> 0
-        testProgressTimer.runAction(to1);
+
+        var to1 = cc.progressTo(3, 0); //trong vòng 5s chạy từ 100-> 0
+
+        var remainingTime = 10;
+        var maxTurnTime = 3;
+
+        var  actionProgress =cc.sequence(
+            cc.spawn(
+                cc.progressTo(remainingTime, 0),
+                cc.tintTo(remainingTime, 255, 0, 0),
+                cc.sequence(
+                    cc.delayTime(remainingTime - Math.min(maxTurnTime * 0.3, remainingTime)),
+                    cc.callFunc(function(){
+                        ZLog.error("do somethingxxxx...");
+                        // Audio.playEffectByLanguage(res.wth_slow);
+                       // Audio.playEffect(res.clocktable, true);
+                       // Vibrator.vibrateDevice(1000);
+                    }),
+                    cc.blink(Math.min(maxTurnTime * 0.3, remainingTime), Math.ceil(maxTurnTime * 0.3 / 0.4))
+                )
+            ),
+            cc.callFunc(function(sender){
+                ZLog.error("do something...");
+                testProgressTimer.setColor(cc.color(0,255,0));
+                testProgressTimer.setPercentage(100); //set giá trị = 100%
+            })
+        );
+        testProgressTimer.runAction(cc.repeatForever(actionProgress));
     },
     sliderEvent: function (sender, type) {
         switch (type){
